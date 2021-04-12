46bbe83921dff3918c51db682837b4f1488e0c64
Vlas Katenev <vlas.katenev@realize.dev>
2021-04-12 22:50:16 +0300

    Поправил стили и классы в html

--- src/js/script.js
+++ src/js/script.js
@@ -5,20 +5,3 @@ function clickMenuButton() {
   }
 
 
-// const mql = window.matchMedia("(max-width: 767px)");
-// mql.addEventListener('change', listenResize);
-// listenResize(mql);
-
-
-// function listenResize(mql) {
-//     const selectElem = document.querySelector('.main-nav__wrapper');
-
-//     if (mql.matches) {
-//         selectElem.classList.add('main-nav--closed');
-//     } else {
-//         selectElem.classList.remove('main-nav--closed');
-//     }
-// }
-
-
-
