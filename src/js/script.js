68752e7c1f01506e84709ce83b71849372ab3ef7
Vlas Katenev <vlas.katenev@realize.dev>
2021-04-13 14:42:51 +0300

    переименовал классы

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 
 function clickMenuButton() {
-    const menu = document.querySelector(".main-nav__wrapper");
-    menu.classList.toggle("main-nav--closed");
+    const menu = document.querySelector(".header-nav__wrapper");
+    menu.classList.toggle("header-nav--closed");
   }
 
 
